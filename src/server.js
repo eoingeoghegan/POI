81dda16932529936f392530b490ab0a822690c8b
Eoin Geoghegan <152919198+eoingeoghegan@users.noreply.github.com>
2025-05-17 15:12:15 +0100

    using render

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 4000,
+    port: process.env.PORT || 4000,
     host: "localhost",
     routes: {
       cors: {
